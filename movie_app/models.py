0bc32e1fe03a87f26627b257eddccdb75fa272f7
EmirKanatov <laft29@mail.ru>
2022-08-19 23:54:19 +0600

    first commit

---
+++ movie_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    duration = models.DurationField()
+    director = models.ForeignKey(to=Director, null=True, on_delete=models.SET_NULL, related_name='movie')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField(null=True, blank=True)
+    movie = models.ForeignKey(to=Movie, null=True, on_delete=models.SET_NULL, related_name='review')
---
+++ movie_app/serializers.py
@@ -0,0 +1,26 @@
+from .models import Director, Review, Movie
+from rest_framework import serializers
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = 'title duration'.split()
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
